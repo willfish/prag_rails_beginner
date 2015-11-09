f13dc5416f3db2fcacb55f8112521995c3c7f000
William Fish <william.fish@mydrivesolutions.com>
2015-11-09 18:12:53 +0000

    Adds additional tests for free and non-free events.

--- events/spec/features/show_event_spec.rb
+++ events/spec/features/show_event_spec.rb
@@ -1,14 +1,22 @@
 describe 'Viewing an individual event' do
   let!(:event) { create(:event) }
+  let!(:free_event) { create(:event, price: 0.0) }
 
-  before(:each) do
+  it "shows the event's details" do
     visit event_url(event)
-  end
-
-  it "it shows the event's details" do
     expect(page).to have_text(event.name)
     expect(page).to have_text(event.location)
     expect(page).to have_text(event.description)
     expect(page).to have_text(event.starts_at)
+  end
+
+  it "shows the price if the price is not 0" do
+    visit event_url(event)
+    expect(page).to have_text(event.price)
+  end
+
+  it "shows 'Free' if the prices is 0" do
+    visit event_url(free_event)
+    expect(page).to have_text("Free")
   end
 end
