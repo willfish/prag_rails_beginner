8c0255661792ee8a43a29c13298d728398892222
William Fish <william.fish@mydrivesolutions.com>
2015-11-14 17:51:46 +0000

    Movies lesson 11 finish.

--- movies/app/models/movie.rb
+++ movies/app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
   FLOP_THRESHOLD = 50_000_000
-  
+  RATINGS = [['U', 1], ['PG', 2], ['12A', 3], ['12', 4], ['15', 5], ['18', 6], ['R18', 7]]
+
   def flop?
     total_gross < FLOP_THRESHOLD
   end
