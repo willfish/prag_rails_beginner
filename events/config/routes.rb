e52130f897829bbe644028ee9e091780ee9694e8
William Fish <william.fish@mydrivesolutions.com>
2015-11-14 18:52:54 +0000

    Adds create/new routes/controllers/views

--- events/config/routes.rb
+++ events/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'events#index'
-  get 'events' => 'events#index'
-  get 'events/:id' => 'events#show', as: 'event'
-  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  # get 'events' => 'events#index'
+  # get 'events/new' => 'events#new', as: 'new_event'
+  # post 'events/create' => 'events/#create', as: 'post_event'
+  # get 'events/:id' => 'events#show', as: 'event'
+  # get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  # patch 'events/:id' => 'events#update'
+  resources :events
 end
--- movies/app/controllers/movies_controller.rb
+++ movies/app/controllers/movies_controller.rb
@@ -10,4 +10,25 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  def create
+    @movie = Movie.new
+  end
+
+  def new
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+
+  def movie_params
+    params.require(:movie).
+      permit(:title, :rating, :total_gross, :description, :released_on)
+  end
 end
